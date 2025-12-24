bae45739c4e4b0a794fe1497b8f53d37eaa1e4ff
Rajarshi Das <rd@rajarshi.me>
2025-12-24 12:39:51 +0530

    setup.sh bugfix

--- setup.sh
+++ setup.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
 # Global
-sudo chown -R rd:rd ..
+sudo chown -R rd:rd .
 sudo find . \( -type d -exec chmod 755 {} \; \) -o \( -type f -exec chmod 644 {} \; \)
-
-# setup script
-chmod +x *.sh
 
 # Writable folder
 sudo chown -R rd:www-data w3filedb
-sudo find w3filedb \( -type d -exec chmod 770 {} \; \) -o \( -type f -exec chmod 660 {} \; \)
+sudo find ./w3filedb \( -type d -exec chmod 770 {} \; \) -o \( -type f -exec chmod 660 {} \; \)
 
 sudo chown -R rd:www-data appengine/w3root/login/box
-sudo find appengine/w3root/login/box \( -type d -exec chmod 775 {} \; \) -o \( -type f -exec chmod 664 {} \; \)
+sudo find ./appengine/w3root/login/box \( -type d -exec chmod 775 {} \; \) -o \( -type f -exec chmod 664 {} \; \)
 
 # Session folder will need to be owned by www-data
 sudo chown -R www-data:rd w3filedb/session
 sudo chown -R www-data:rd w3filedb/log
 
+# bash scripts
+chmod +x *.sh
 sudo chmod +x ./w3filedb/*.sh
 
