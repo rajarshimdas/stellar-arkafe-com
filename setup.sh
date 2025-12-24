946f85b414720a74a3842e0fef8b6edefc79180d
Rajarshi Das <rd@rajarshi.me>
2025-12-24 11:46:05 +0530

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Global
 sudo chown -R rd:rd ..
-sudo find .. \( -type d -exec chmod 755 {} \; \) -o \( -type f -exec chmod 644 {} \; \)
+sudo find . \( -type d -exec chmod 755 {} \; \) -o \( -type f -exec chmod 644 {} \; \)
 
 # setup script
 chmod +x *.sh
@@ -15,5 +15,8 @@ sudo chown -R rd:www-data appengine/w3root/login/box
 sudo find appengine/w3root/login/box \( -type d -exec chmod 775 {} \; \) -o \( -type f -exec chmod 664 {} \; \)
 
 # Session folder will need to be owned by www-data
-sudo chown -R www-data:www-data w3filedb/session
-sudo chown -R www-data:www-data w3filedb/log
+sudo chown -R www-data:rd w3filedb/session
+sudo chown -R www-data:rd w3filedb/log
+
+sudo chmod +x ./w3filedb/*.sh
+
